60b659523183d36f2fafb8a27154130e13d13b2d
Paulo <paulo@email.com>
2026-06-26 15:30:57 -0300

    aplicação do docker

--- app/settings.py
+++ app/settings.py
@@ -90,7 +90,17 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sge',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'sge_db',
+        'PORT': '5432',
+    },
+    
+    'dev': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
